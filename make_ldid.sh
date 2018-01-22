212a119d69fffcc7fc752ffde536a0feb8c69354
Gary <garynil1635@gmail.com>
2018-01-22 23:27:43 +0800

    Supported newest Xcode (9.2) and add make script of ldid2

---
+++ make_ldid.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+sudo xcode-select --switch /Applications/Xcode.app
+
+if which xcrun &>/dev/null; then
+    flags=(xcrun -sdk macosx g++)
+    flags+=(-mmacosx-version-min=10.4)
+
+    for arch in i386 x86_64; do
+        flags+=(-arch "${arch}")
+    done
+else
+    flags=(g++)
+fi
+
+flags+=(-I.)
+
+set -x
+"${flags[@]}" -c -std=c++11 -o ldid.o ldid.cpp
+"${flags[@]}" -o ldid ldid.o -x c lookup2.c -x c sha1.c
--- make_ldid2.sh
+++ make_ldid2.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-sudo xcode-select --switch /Applications/Xcode-5.1.1.app
+sudo xcode-select --switch /Applications/Xcode.app
 
 if which xcrun &>/dev/null; then
     flags=(xcrun -sdk macosx g++)
-    flags+=(-mmacosx-version-min=10.4)
+    flags+=(-mmacosx-version-min=10.9)
 
     for arch in i386 x86_64; do
         flags+=(-arch "${arch}")
@@ -18,5 +18,5 @@ fi
 flags+=(-I.)
 
 set -x
-"${flags[@]}" -c -std=c++11 -o ldid.o ldid.cpp
-"${flags[@]}" -o ldid ldid.o -x c lookup2.c -x c sha1.c
+"${flags[@]}" -c -std=c++11 -o ldid.o ldid2.cpp
+"${flags[@]}" -o ldid2 ldid.o -x c lookup2.c -x c sha1.c sha224-256.c
